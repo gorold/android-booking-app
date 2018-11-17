3519a7ad3eaf13cdbbcf17c368ff81028b191efe
Chin <chinstermojo@gmail.com>
2018-11-17 22:49:41 +0800

    Booking Screen + Calendar

--- Java1DApp/app/src/main/java/com/gerald/java1dapp/MainActivity.java
+++ Java1DApp/app/src/main/java/com/gerald/java1dapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.gerald.java1dapp;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "mainBooking";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void logIn(View view) {
+        startActivity(new Intent(this,bookingScreen.class));
     }
 }
